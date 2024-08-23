bf43c1015329b0c9e011ed954fe8fd50cfe4cb39
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-08-23 23:49:58 +0630

    design columncontainer,add and delete column

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import PlusIcon from "../icons/PlusIcon"
-import { Column } from "../Types";
+import { Column, Id } from "../Types";
+import ColumnContainer from "./ColumnContainer";
 
 function KanbanBoard() {
 
@@ -18,15 +19,26 @@ function KanbanBoard() {
       return Math.floor(Math.random()*1000);
   }
 
+  function deleteColumn(id:Id){
+    const filteredColumn = columns.filter(col=>col.id != id);
+    setColumns(filteredColumn);
+  }
+  
+
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center justify-center overflow-x-auto overflow-y-hidden px-[40px]">
-        <>
-            <button onClick={()=>createColumn} className="h-[60px] w-[350px] min-w-[350px] cursor-pointern rounded-lg bg-mainBackgroundColor border-2 border-coloumnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+              {columns.map(column=> 
+                  <ColumnContainer key={column.id} column={column} deleteColumn={deleteColumn}/>
+              )}
+          </div>
+            <button onClick={()=>createColumn()} className="h-[60px] w-[350px] min-w-[350px] cursor-pointern rounded-lg bg-mainBackgroundColor border-2 border-coloumnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
                 <PlusIcon/>
                 Add Column
             </button>
-        </>
+        </div>
     </div>
 
   )
