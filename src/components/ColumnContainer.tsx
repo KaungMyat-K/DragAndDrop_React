a0ab62690acfaa32fbc9328a28d19964d91269ed
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-08-24 23:49:55 +0630

    design task component and create task

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,17 +1,23 @@
 import { useSortable } from "@dnd-kit/sortable";
 import TrashIcon from "../icons/TrashIcon";
-import { Column, Id } from "../Types"
+import { Column, Id, Task } from "../Types"
 import { CSS } from '@dnd-kit/utilities'
 import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import TaskCard from "./TaskCard";
 
 interface Props{
     column : Column;
+    tasks : Task[];
     deleteColumn : (id: Id) => void;
+    updateColumn : (id: Id,title: string) => void;
+    createTask : (columnId: Id) => void;
+    deleteTask : (id:Id)=>void;
 }
 
 function ColumnContainer(props : Props) {
 
-    const {column,deleteColumn} = props;
+    const {column,deleteColumn,updateColumn,createTask,tasks,deleteTask} = props;
     const [editMode,setEditMode] = useState(false);
 
     const {setNodeRef,attributes,listeners,transform,transition,isDragging} =  useSortable({
@@ -19,7 +25,8 @@ function ColumnContainer(props : Props) {
         data : {
             type : "Column",
             column
-        }
+        },
+        disabled: editMode
     })
 
     const style = {
@@ -27,9 +34,7 @@ function ColumnContainer(props : Props) {
         transform: CSS.Transform.toString(transform),
     }
 
-    function updateColumn(title: Column){
 
-    }
 
     if(isDragging){
         return (
@@ -54,19 +59,19 @@ function ColumnContainer(props : Props) {
                                 if(e.key !== "Enter") return;
                                 setEditMode(false);
                              }} 
-                            className="bg-black focus:"
+                            className="bg-black focus:border-rose-500 border rounded outline-none px-2"
                     />}
             </div>
             <button onClick={()=>deleteColumn(column.id)}  className="stroke-gray-500 hover:stroke-white hover:bg-coloumnBackgroundColor rounded px-1 py-2">
                 <TrashIcon/>
             </button>
         </div>
-        <div className="flex flex-grow">
-            Content    
+        <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto ">
+            {tasks.map(task=>(
+                <TaskCard key={task.id} task={task} deleteTask={deleteTask}/>
+            ))}    
         </div>
-        <div>
-            footer
-        </div>
+        <button onClick={()=>createTask(column.id)} className="flex gap-2 items-center border-coloumnBackgroundColor border-2 rounded-md p-4 border-x-coloumnBackgroundColor hover:text-rose-400 active:bg-black"><PlusIcon/> Add Task</button>
     </div>
   )
 }
--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react"
 import PlusIcon from "../icons/PlusIcon"
-import { Column, Id } from "../Types";
+import { Column, Id, Task } from "../Types";
 import ColumnContainer from "./ColumnContainer";
 import { DndContext, DragEndEvent, DragOverlay, DragStartEvent, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { arrayMove, SortableContext } from "@dnd-kit/sortable";
@@ -12,6 +12,7 @@ function KanbanBoard() {
   const [columns,setColumns] = useState<Column[]>([]);
   const columnsId = useMemo(()=> columns.map(col => col.id),[columns]);
   const [activeColumn,setActiveColumn] = useState<Column | null>(null);
+  const [tasks,setTasks] = useState<Task[]>([]);
   const sensors = useSensors(
     useSensor(PointerSensor,{
       activationConstraint:{
@@ -36,6 +37,28 @@ function KanbanBoard() {
     const filteredColumn = columns.filter(col=>col.id != id);
     setColumns(filteredColumn);
   }
+
+  function updateColumn(id:Id,title:string){
+    const newColumns = columns.map(col=>{
+      if(col.id !== id) return col;
+      return {...col,title};
+    })
+    setColumns(newColumns);
+  }
+
+  function creatTask(columnId: Id){
+    const newTask : Task = {
+      id: generateId(),
+      columnId,
+      content: `Task ${tasks.length}`
+    }
+    setTasks([...tasks,newTask]);
+  }
+
+  function deleteTask(id: Id){
+    const newTasks = tasks.filter(task=> task.id !== id);
+    setTasks(newTasks);
+  }
   
   function onDragStart(event: DragStartEvent){
     if(event.active.data.current?.type === "Column"){
@@ -58,13 +81,13 @@ function KanbanBoard() {
   }
 
   return (
-    <div className="m-auto flex min-h-screen w-full items-center justify-center overflow-x-auto overflow-y-hidden px-[40px]">
+    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
       <DndContext onDragStart={onDragStart} onDragEnd={onDragEnd} sensors={sensors}>
         <div className="m-auto flex gap-4">
           <div className="flex gap-4">
             <SortableContext items={columnsId}>
                 {columns.map(column=> 
-                  <ColumnContainer key={column.id} column={column} deleteColumn={deleteColumn}/>
+                  <ColumnContainer key={column.id} column={column} deleteColumn={deleteColumn} updateColumn={updateColumn} createTask={creatTask} tasks={tasks.filter(task=>task.columnId === column.id)} deleteTask={deleteTask}/>
                 )}
               </SortableContext>
               </div>
@@ -75,7 +98,9 @@ function KanbanBoard() {
         </div>
         {createPortal(
           <DragOverlay>
-            {activeColumn && <ColumnContainer column={activeColumn} deleteColumn={deleteColumn}/>}
+            {activeColumn && 
+            <ColumnContainer column={activeColumn} deleteColumn={deleteColumn} updateColumn={updateColumn} createTask={creatTask} tasks={tasks.filter(task=>task.columnId === activeColumn.id)} deleteTask={deleteTask}/>
+            }
           </DragOverlay>
           ,document.body
         )}
